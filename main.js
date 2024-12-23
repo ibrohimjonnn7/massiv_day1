02c5c5a8ab74fac078a3040ebdde54c6b737f6d2
Davlatov Ibroghim <ibrohimjonnn.77>
2024-12-23 08:30:40 +0500

    day1

---
+++ main.js
@@ -0,0 +1,56 @@
+// function myfunc(...a){
+// let sum = 0;
+// for(let i = 0; i<a.length; i++){
+//      sum += a[i];
+// }
+// return sum;
+// }
+
+// console.log(myfunc(2,3,4,5,6));
+
+// function myfunc(a) {
+//   let maxx = -9999999;
+//   for (let i = 0; i < a.length; i++) {
+//     if(a[i] > maxx ){
+//         maxx = a[i];
+//     }
+//   }
+//   return maxx;
+// }
+
+// console.log(myfunc([12, 11, 34, 15, 40, 43, 7]));
+
+// function myfunc(a){
+//  sum = 0;
+//  for(i = 0; i < a.length; i++){
+//     if (a[i] % 2 == 0){
+//         sum++;
+//     }
+//  }
+//  return sum
+// }
+// console.log(myfunc([1,3,4,6,7,10]));
+
+
+
+// function myfunc(...a){
+// let sum = 0;
+// for(let i = 0; i<a.length; i++){
+//      sum += a[i] / 2;
+// }
+// return sum;
+// }
+
+// console.log(myfunc(2,3,4,5,6,21));
+
+// function myfunc(a,b){
+//     for(i = 0; i <= a.length; i++){
+
+//     }
+//     if (a[i] == b) {
+//       return "true";
+//     } else {
+//       return "false";
+//     }
+// }
+// console.log(myfunc([1,2,3,4,5,6],4));
